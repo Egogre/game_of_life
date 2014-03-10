293f2def4c52000f365a7b9bcb1d523562837afc
Will Mitchell <wvmitchell@gmail.com>
2014-03-10 13:18:49 -0600

    initial working implementation

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -6,11 +6,19 @@ class GameOfLife
   def initialize(options={})
     rows = options[:rows] || 3
     cols = options[:columns] || 3
-    generate(rows, cols)
+    @cells = generate(rows, cols)
   end
 
   def board
     cells.collect {|row| row.join(' ')}.join("\n")
+  end
+
+  def populate(options)
+    options[:cells].each do |pair|
+      row = pair[:row]
+      column = pair[:column]
+      cells[row][column] = 'X'
+    end
   end
 
   def make_live(options={})
@@ -27,24 +35,40 @@ class GameOfLife
     cells[row][column] = 'O'
   end
 
-  #def run
-  #  system 'clear' or system 'cls'
-  #  5.times do
-  #    puts board
-  #    sleep 1
-  #    system 'clear' or system 'cls'
-  #  end
-  #end
+  def alive?(options)
+    row = options[:row]
+    column = options[:column]
+    cells[row][column] == 'X'
+  end
+
+  def cycle
+    counts = living_neighbors
+    cells.each_with_index do |row, i|
+      row.each_index do |j|
+        update(row: i, column: j, neighbors: counts[i][j])
+      end
+    end
+  end
+
+  def run
+    system 'clear' or system 'cls'
+    5.times do
+      puts board
+      sleep 1
+      system 'clear' or system 'cls'
+    end
+  end
 
   private
 
   def generate(rows, cols)
-    @cells = Array.new(rows) {Array.new(cols)}
+    cells = Array.new(rows) {Array.new(cols)}
     cells.each_with_index do |row, i|
       row.each_index do |j|
         cells[i][j] = "O"
       end
     end
+    cells
   end
 
   def raise_if_invalid(options)
@@ -54,11 +78,52 @@ class GameOfLife
   end
 
   def illegal_row(row)
-    row > cells.length
+    row > cells.length-1 || row < 0
   end
 
   def illegal_column(column)
-    column > cells.first.length
+    column > cells.first.length-1 || column < 0
+  end
+
+  def living_neighbors
+    living_neighbors_count = generate(cells.length, cells.first.length)
+    cells.each_with_index do |row, i|
+      row.each_index do |j|
+        living_neighbors_count[i][j] = num_living_neighbors(row: i, column: j)
+      end
+    end
+    living_neighbors_count
+  end
+
+  def num_living_neighbors(options)
+    row = options[:row]
+    column = options[:column]
+    count = 0
+    (row-1..row+1).to_a.each do |r|
+      (column-1..column+1).to_a.each do |c|
+        unless illegal_row(r) || illegal_column(c) || same_cell(row, column, r, c)
+          count += 1 if alive?(row: r, column: c)
+        end
+      end
+    end
+    count
+  end
+
+  def same_cell(row, col, test_row, test_col)
+    row == test_row && col == test_col
+  end
+
+  def update(options)
+    row = options[:row]
+    column = options[:column]
+    neighbors = options[:neighbors]
+    if neighbors < 2
+      cells[row][column] = 'O'
+    elsif neighbors == 3
+      cells[row][column] = 'X'
+    elsif neighbors > 3
+      cells[row][column] = 'O'
+    end
   end
 
 end
--- test/game_of_life_test.rb
+++ test/game_of_life_test.rb
@@ -30,9 +30,30 @@ class GameOfLifeTest < MiniTest::Test
     assert_equal "O O O\nO O O\nO O O", game.board
   end
 
+  def test_a_cell_can_be_queried
+    game.make_live(row: 0, column: 1)
+    assert game.alive?(row: 0, column: 1)
+    refute game.alive?(row: 0, column: 2)
+  end
+
+  def test_game_can_be_given_starting_conditions
+    game.populate(cells: [{row:0, column:1}, {row:1, column:0}, {row:2, column:1}])
+    assert_equal "O X O\nX O O\nO X O", game.board
+  end
+
   def test_out_of_bounds_raises_error
     assert_raises(ArgumentError) {game.make_live(row: 5, column: 5)}
+    assert_raises(ArgumentError) {game.make_live(row: -5, column: -15)}
     assert_raises(ArgumentError) {game.make_dead(row: 5, column: 5)}
+    assert_raises(ArgumentError) {game.make_dead(row: -5, column: -5)}
+  end
+
+  def test_game_cycles_correctly
+    game.populate(cells: [{row:0, column:2}, {row:1, column:0}, {row:2, column:1}])
+    game.cycle
+    assert_equal "O O O\nO X O\nO O O", game.board
+    game.cycle
+    assert_equal "O O O\nO O O\nO O O", game.board
   end
 
 
