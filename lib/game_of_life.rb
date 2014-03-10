dff4227c782d80c1e9f3905648e368f384b4c211
Will Mitchell <wvmitchell@gmail.com>
2014-03-10 11:40:18 -0600

    board of arbitrary size can now be created and printed

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,12 +1,25 @@
+require 'pry'
 class GameOfLife
 
-  def initialize(options=nil)
+  attr_reader :cells
+
+  def initialize(options={})
     rows = options[:rows] || 3
     cols = options[:columns] || 3
+    generate(rows, cols)
   end
 
   def board
-    "O O O\nO O O\nO O O"
+    cells.collect {|row| row.join(' ')}.join("\n")
+  end
+
+  def generate(rows, cols)
+    @cells = Array.new(rows, Array.new(cols))
+    cells.each_with_index do |row, i|
+      row.each_index do |j|
+        cells[i][j] = "O"
+      end
+    end
   end
 
 end
--- test/game_of_life_test.rb
+++ test/game_of_life_test.rb
@@ -8,12 +8,15 @@ class GameOfLifeTest < MiniTest::Test
 
   def test_a_blank_default_board_is_generated
     @game = GameOfLife.new
-    assert_equal game.board, "O O O\nO O O\nO O O"
+    assert_equal "O O O\nO O O\nO O O", game.board
   end
 
   def test_a_blank_board_of_arbitrary_size_is_generated
     @game = GameOfLife.new(rows: 3, columns: 4)
-    assert_equal game.board, "O O O O\nO O O O\nO O O O\nO O O O"
+    assert_equal "O O O O\nO O O O\nO O O O", game.board
+  end
+
+  def test_living_cell_can_be_placed
   end
 
 end
